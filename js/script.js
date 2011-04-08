feab3d508a6bb285ca4839af659b0209fceea38f
Jake Lear <jakelear@gmail.com>
2011-04-07 22:37:55 -0400

    Beginning version 2

--- js/script.js
+++ js/script.js
@@ -1,48 +1,170 @@
-// Radiation Dose Chart 
-// by Jake Lear
+/* Author: Jake Lear
 
-var RADIATION = {
-	
-	getScrollStyles: function(scroll){
-		//Set up scale 
-		var scale = Math.pow( 1/21,  scroll * 3 ),
-	      	prop = 'scale(' + scale + ')',
-			scrolled = 0,
-	      	style = {
-	        	WebkitTransform : prop,
-	        	MozTransform : prop,
-	       		OTransform : prop,
-	        	transform : prop
-	      	};
- 		return style;
+*/
+
+Raphael.fn.connection = function(obj1, obj2, line, bg) {
+    if (obj1.line && obj1.from && obj1.to) {
+        line = obj1;
+        obj1 = line.from;
+        obj2 = line.to;
+    }
+    var bb1 = obj1.getBBox(),
+        bb2 = obj2.getBBox(),
+        p = [{
+            x: bb1.x + bb1.width / 2,
+            y: bb1.y - 1
+        }, {
+            x: bb1.x + bb1.width / 2,
+            y: bb1.y + bb1.height + 1
+        }, {
+            x: bb1.x - 1,
+            y: bb1.y + bb1.height / 2
+        }, {
+            x: bb1.x + bb1.width + 1,
+            y: bb1.y + bb1.height / 2
+        }, {
+            x: bb2.x + bb2.width / 2,
+            y: bb2.y - 1
+        }, {
+            x: bb2.x + bb2.width / 2,
+            y: bb2.y + bb2.height + 1
+        }, {
+            x: bb2.x - 1,
+            y: bb2.y + bb2.height / 2
+        }, {
+            x: bb2.x + bb2.width + 1,
+            y: bb2.y + bb2.height / 2
+        }],
+        d = {},
+        dis = [];
+    for (var i = 0; i < 4; i++) {
+        for (var j = 4; j < 8; j++) {
+            var dx = Math.abs(p[i].x - p[j].x),
+                dy = Math.abs(p[i].y - p[j].y);
+            if ((i == j - 4) || (((i != 3 && j != 6) || p[i].x < p[j].x) && ((i != 2 && j != 7) || p[i].x > p[j].x) && ((i != 0 && j != 5) || p[i].y > p[j].y) && ((i != 1 && j != 4) || p[i].y < p[j].y))) {
+                dis.push(dx + dy);
+                d[dis[dis.length - 1]] = [i, j];
+            }
+        }
+    }
+    if (dis.length == 0) {
+        var res = [0, 4];
+    } else {
+        res = d[Math.min.apply(Math, dis)];
+    }
+    var x1 = p[res[0]].x,
+        y1 = p[res[0]].y,
+        x4 = p[res[1]].x,
+        y4 = p[res[1]].y;
+    dx = Math.max(Math.abs(x1 - x4) / 2, 10);
+    dy = Math.max(Math.abs(y1 - y4) / 2, 10);
+    var x2 = [x1, x1, x1 - dx, x1 + dx][res[0]].toFixed(3),
+        y2 = [y1 - dy, y1 + dy, y1, y1][res[0]].toFixed(3),
+        x3 = [0, 0, 0, 0, x4, x4, x4 - dx, x4 + dx][res[1]].toFixed(3),
+        y3 = [0, 0, 0, 0, y1 + dy, y1 - dy, y4, y4][res[1]].toFixed(3);
+    var path = ["M", x1.toFixed(3), y1.toFixed(3), "C", x2, y2, x3, y3, x4.toFixed(3), y4.toFixed(3)].join(",");
+    if (line && line.line) {
+        line.bg && line.bg.attr({
+            path: path
+        });
+        line.line.attr({
+            path: path
+        });
+    } else {
+        var color = typeof line == "string" ? line : "#000";
+        return {
+            bg: bg && bg.split && this.path(path).attr({
+                stroke: bg.split("|")[0],
+                fill: "none",
+                "stroke-width": bg.split("|")[1] || 3
+            }),
+            line: this.path(path).attr({
+                stroke: color,
+                fill: "none"
+            }),
+            from: obj1,
+            to: obj2
+        };
+    }
+};
+
+
+
+var r = new Raphael("homepage-vis"),
+	connections = [],
+	bubbles = [r.circle(130, 130, 2000000000),r.circle(130, 130, 2000000),r.circle(130, 130, 2000),r.circle(130, 130, 100)];
+	unitConnector = r.circle(296, 70, 1).attr({fill:"#fff", stroke:"#FFF"});
+	bubbles[0].attr({fill: "#FFDC62", stroke: "#FFDC62"});
+	bubbles[1].attr({fill: "#89DAFA", stroke: "#89DAFA"});
+	bubbles[2].attr({fill: "#C2CC1E", stroke: "#C2CC1E"});
+	bubbles[3].attr({fill: "#D91895", stroke: "#D91895"});
+	//for(var i = 0; i < bubbles.length; i +=1){
+	connections[3] = r.connection(bubbles[3], unitConnector, "#fff", "#fff");
+	 var link;
+	//}	
+var RADS = {
+	RNG: function(min,max){
+		return Math.floor(Math.random() * (max - min + 1)) + min;
 	},
 	
-	scrollZoom: function(){
-		//cache some elements
-		var $content = $('#content'),
-	  		$title = $('#title'),
-	  		$nav = $('#nav'),
-			$body = $('body'),
-			$window = $(window),
-			$document = $(document);
-			
-		// normalize scroll value from 0 to 1
-		scrolled = $window.scrollTop() / ($document.height() - $window.height() );
-		transformScroll(scrolled);
-		
-		function transformScroll(scroll){
-			$content.css( RADIATION.getScrollStyles( scroll ) );
-		}
 
-	}	
+	getScrollScale: function(scroll) {
+	        //Set up scale 
+	        var scale = Math.pow(1/65, scroll * 4); 
+	        return scale;
+	},
+	scrollZoom: function() {
+	        //cache some elements
+	        var $window = $(window),
+	            $document = $(document);
 	
+	        // normalize scroll value from 0 to 1
+	        scrolled = $window.scrollTop() / ($document.height() - $window.height());
+	        transformScroll(scrolled);
+			
+			
+	        function transformScroll(scroll) {
+				var links = [];
+				for (var i = 0; i<bubbles.length; i+=1){
+					bubbles[i].scale(RADS.getScrollScale(scroll),RADS.getScrollScale(scroll));
+					//console.log(scroll);
+				}		
+				link = r.connection(connections[3]);	
+				var $unitTitle = $('#unit-representation'),
+					$unitText = $unitTitle.html();
+				
+					if (scroll < 0.155 && $unitText != '0.05 &mu;Sv'){
+						$unitTitle.html('0.05 &mu;Sv'); 
+						
+					} else if (scroll > 0.155 && scroll < 0.547 && $unitText != '1 &mu;Sv'){
+						$unitTitle.html('1 &mu;Sv');
+						
+					} else if (scroll > 0.547 && scroll < 0.942 && $unitText != '1 mSv') {
+						$unitTitle.html('1 mSv');
+					} else if (scroll > 0.942 && $unitText != '1 Sv'){
+						$unitTitle.html('1 Sv');
+					}
+	        
+	    }
+	}
 }
+	$(function() {
+		//RADS.initViz();
+	    $(window).scroll(function() {
+	        RADS.scrollZoom();
+			
+	    });
+	});
+		
 
-$(function(){
-	$(window).scroll(function() {
-	        RADIATION.scrollZoom();
-	 });
-});
+	
+
+
+
+
+
+
+
 
 
 
